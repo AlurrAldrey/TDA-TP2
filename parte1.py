c2bc4b109461486cda2f132955e0eea5fc739ffb
AlurrAldrey <48604870+AlurrAldrey@users.noreply.github.com>
2021-05-29 15:10:17 -0300

    resolución parte 1 con muchos prints que tengo que sacar

---
+++ parte1.py
@@ -0,0 +1,80 @@
+def main():
+    
+    print("ingrese el nombre del archivo de cartas:")
+    filename = input()
+    file = open(filename,"r")
+    
+    linea = file.readline()
+    cartas = linea.split(",")
+    
+    n = 0
+    for i,carta in enumerate(cartas):
+        cartas[i] = int(carta)
+        n += 1
+
+    opt = [[0]*n for i in range(n)]
+    elecciones = [[0]*n for i in range(n)]
+    print(opt)
+
+    k = 0
+    while k < n:
+        izq=0
+        der=k
+        while(der < n):
+
+            if izq == der:
+                opt[izq][der] = cartas[izq]
+                elecciones[izq][der] = izq
+            else:
+            
+                carta_izq = (opt[izq][izq] - opt[izq+1][der])
+                carta_der = (opt[der][der] - opt[izq][der-1])
+
+                if carta_izq > carta_der:
+                    elecciones[izq][der] = izq
+                    opt[izq][der] = carta_izq
+                else:
+                    elecciones[izq][der] = der
+                    opt[izq][der] = carta_der
+                #opt[izq][der] = max(carta_izq, carta_der)
+            
+            print((izq,der),opt[izq][der])
+            izq += 1
+            der += 1
+        k += 1
+
+    print("optimos:")
+    for fila in opt:
+        print(fila)
+    
+    print("elecciones:")
+    for fila in elecciones:
+        print(fila)
+
+    izq = 0
+    der = n-1
+    jugador = 0
+    resultado = [[],[]] #cartas que eligió cada jugador
+    puntajes = [0,0] #puntos de cada jugador
+
+    while izq<=der:
+        
+        eleccion = elecciones[izq][der] #lado que elije
+        carta_elegida = cartas[eleccion] #la carta que estaba en ese lado
+        resultado[jugador].append(carta_elegida)
+        puntajes[jugador] += carta_elegida
+        jugador = (jugador+1)%2 #cambio de jugador
+        
+        if eleccion == izq:
+            izq+=1
+        else:
+            der-=1
+        
+    print(resultado)
+    print(puntajes)
+
+    
+
+
+main()
+        
